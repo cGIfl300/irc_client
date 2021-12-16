eae0913f994d4ce0308f63157f848c450225a140
cGIfl300 <cgifl300@gmail.com>
2021-12-16 17:41:19 +0100

    The client can now disconnect properly

--- test_client.py
+++ test_client.py
@@ -1,11 +1,24 @@
 from irc import IRC
 
-irc = IRC("irc.swiftirc.net", "#FreshChat", "TestBOT938")
+irc = IRC("irc.swiftirc.net", "#HereIsTheTestchannel", "TestBOT938")
 irc.connect()
 
-while True:
+exit_trigger = False
+
+while not exit_trigger:
     response = irc.get_response()
     for line in response:
         print(line)
-        if "PRIVMSG" in line and irc.channel in line and "hello" in line:
-            irc.send("Hello!")
+
+        if "PRIVMSG" in line:
+            if "hello" in line:
+                print("I see an hello!")
+                irc.send("Hello!\n")
+            if "disconnect" in line:
+                print("I have to disconnect baby.")
+                irc.disconnect()
+                exit_trigger = True
+
+# To test, send "hello" it should reply "Hello!"
+# Send nothing it shouldn't act
+# Send "disconnect" to disconnect the bot, it should have a default quit message
