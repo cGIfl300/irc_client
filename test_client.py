d9f6cf9716305ac2864710d72a18f66b139e606a
cGIfl300 <cgifl300@gmail.com>
2022-01-01 21:42:49 +0100

    Get users list

--- test_client.py
+++ test_client.py
@@ -1,20 +1,19 @@
 from irc import IRC
 from split_raw_message import split_raw_message
 
-irc = IRC("irc.swiftirc.net", "#HereIsTheTestchannel", "TestBOT938")
+irc = IRC("irc.executingreality.com", "#test9302", "bot_name")
 irc.connect()
 
 exit_trigger = False
 message = ""
+fortune = ""
+secure_nick = "cGIfl300"
 
 while not exit_trigger:
 
     response = irc.get_response()
 
     for line in response:
-
-        # print(f"RAW: {line}")
-
         message = split_raw_message(line)
 
         if message is None:
@@ -25,24 +24,39 @@ while not exit_trigger:
         if message["type"] == "352":
             irc.users_list.append(message)
 
+        if message["type"] == "315":
+            print("Users list completed.")
+            irc.users_list_completed = True
+            continue
+
         if message["type"] == "PRIVMSG":
             sender_nickname = message["sender_nickname"]
+        else:
+            continue
 
-            if message["body"].find("hello") == 0:
-                print("I see an hello!")
-                irc.send(f"Hello! {sender_nickname}\n")
+        if message["body"].find("$version") == 0:
+            irc.send(
+                "I am a bot, based on https://github.com/cGIfl300/irc_client .")
 
-            if message["body"].find("disconnect") == 0:
-                print("I have to disconnect baby.")
-                irc.disconnect()
-                exit_trigger = True
+        if message["body"].find("$help") == 0:
+            irc.send("Available: hello, cookie, disconnect")
 
-            if message["body"].find("get_users_list") == 0:
-                print("Time has come to get the list of users.")
-                irc.users_list = []
-                irc.get_users_list()
+        if message["body"].find("$hello") == 0:
+            print("I see an hello!")
+            irc.send(f"Hello! {sender_nickname}")
 
-# To test, send "hello" it should reply "Hello! (your nickname)"
-# Send nothing it shouldn't act
-# Send "disconnect" to disconnect the bot, it should have a default quit
-# message
+        if not message["sender_nickname"] == secure_nick:
+            continue
+
+        # These commands are restricted to the secure_nick
+
+        if message["body"].find("$disconnect") == 0:
+            print("I have to disconnect baby.")
+            irc.disconnect()
+            exit_trigger = True
+
+        if message["body"].find("$get_users_list") == 0:
+            print("Time has come to get the list of users.")
+            irc.users_list_completed = False
+            irc.users_list = []
+            irc.get_users_list()
